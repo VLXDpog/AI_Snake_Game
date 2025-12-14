6c9502d7796d9869007e535b303b247f789181e0
VLXDpog <Daungocduc2006@gmail.com>
2025-12-14 19:09:35 +0700

    Update snake_game_env.py
    change some rules

--- snake_game_env.py
+++ snake_game_env.py
@@ -300,4 +300,6 @@ class SnakeGameVersus:
         elif self.direction2 == Direction.LEFT: x -= BLOCK_SIZE
         elif self.direction2 == Direction.DOWN: y += BLOCK_SIZE
         elif self.direction2 == Direction.UP: y -= BLOCK_SIZE
+
         self.head2 = Point(x, y)
+        #may say thang hoa
